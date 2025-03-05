438e37cae69db30ace4454f1f4702cc4e2e1bd31
yehan21yas <yasoveryone@gmail.com>
2025-03-05 15:15:36 +0530

    title changed

--- src/main/java/lk/acpt/demofx/HelloApplication.java
+++ src/main/java/lk/acpt/demofx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Lol!");
+        stage.setTitle("Login page!");
         stage.setScene(scene);
         stage.show();
     }
